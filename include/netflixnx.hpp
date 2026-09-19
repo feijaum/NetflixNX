d379663bc05e563a67636ad6d7fd46be43c9b60a
def joao_victor <jvleite7@gmail.com>
2026-09-19 17:57:03 -0300

    Expand app state for TV UI, details and player

--- include/netflixnx.hpp
+++ include/netflixnx.hpp
@@ -4,11 +4,18 @@
 
 namespace nx {
 enum class Screen { Home, Search, MyNetflix, Settings };
+enum class View { Browse, Details, Player };
 
 struct AppState {
     Screen screen = Screen::Home;
+    View view = View::Browse;
     int focusedCard = 0;
+    int focusedRow = 0;
+    int detailsAction = 0;
+    bool playerPaused = false;
     bool running = true;
+    bool touchWasDown = false;
+    Uint32 lastInputTick = 0;
 };
 
 void handleInput(AppState& state, PadState& pad, SDL_Renderer* renderer);
