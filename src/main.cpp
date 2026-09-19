39f1da2bbd1f6cbe5cdd5ee90050607449b39b45
def joao_victor <jvleite7@gmail.com>
2026-09-19 17:59:24 -0300

    Add frame delta update loop for UI animation

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,29 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "NetflixNX", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        1280, 720, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(
-        window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Window* window = SDL_CreateWindow("NetflixNX",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     PadState pad;
     padConfigureInput(1, HidNpadStyleSet_NpadStandard);
     padInitializeDefault(&pad);
 
     nx::AppState state;
+    Uint32 previous = SDL_GetTicks();
     while (appletMainLoop() && state.running) {
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) state.running = false;
-        }
+        while (SDL_PollEvent(&event)) if (event.type == SDL_QUIT) state.running = false;
+
+        Uint32 now = SDL_GetTicks();
+        float dt = (now - previous) / 1000.0f;
+        previous = now;
+        if (dt > 0.05f) dt = 0.05f;
 
         padUpdate(&pad);
         nx::handleInput(state, pad, renderer);
+        nx::update(state, dt);
         nx::render(renderer, state);
         SDL_RenderPresent(renderer);
     }
